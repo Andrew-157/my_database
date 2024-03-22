9ef8f0ec0ef69f9401386b9c640c6bc096ba757f
Andrew Subotin <subotinandrey5@gmail.com>
2024-03-22 22:01:57 +0100

    Maybe session.py is not that necessary after all - at least, for now.

--- db_manager.py
+++ db_manager.py
@@ -1,2 +1,19 @@
+#!/usr/bin/python3
+
+import datetime
 
 from db_manager_configuration import DBManagerConfiguration
+from parse import Parser
+
+
+def run(parser):
+    console_string = f"[({DBManagerConfiguration.MODE})\
+            {datetime.strftime(datetime.now(), '%Y-%m-%d:%H-%M')}]"
+    while True:
+        parser.parse(input(console_string))
+
+
+if __name__ == "__main__":
+    print("Starting DB Manager...")
+    parser = Parser()
+    run(parser)
